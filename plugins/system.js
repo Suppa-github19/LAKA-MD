0150854743676d32782aca83f5f15b0d0072e801
Laka <149492604+Suppa-github19@users.noreply.github.com>
2024-08-21 15:55:19 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -15,7 +15,7 @@ try{
 let status = `*Uptime:*  ${runtime(process.uptime())}
 *Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
 *HostName:* ${os.hostname()}
-*Owner:* Prabath Kumara & Supun Madhuranga
+*Owner:* Supun Madhuranga
 `
 return reply(`${status}`)
 }catch(e){
