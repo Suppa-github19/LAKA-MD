100021df4789af4bc5f51ccd2e41dd91a0618c48
Laka <149492604+Suppa-github19@users.noreply.github.com>
2024-08-21 16:04:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ require("./plugins/" + plugin);
 console.log('Plugins installed successful ✅')
 console.log('Bot connected to whatsapp ✅')
 
-let up = `Laka-md connected successful ✅\n\nPREFIX: ${prefix}`;
+let up = `Laka-md bot connected successful ✅\n\nPREFIX: ${prefix}`;
 
 conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/20814386ac8526c7d91aa.jpg` }, caption: up })
 
