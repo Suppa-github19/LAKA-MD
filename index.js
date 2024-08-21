cd50fb574b466b0214638ec5b832973289a09fcb
Laka <149492604+Suppa-github19@users.noreply.github.com>
2024-08-21 17:36:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94768593158','94755250046']
+const ownerNumber = ['94768593158']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
